c00f81564ff8e1056714165060f246d699c05f2c
Mohansinghpawar <88230569+Mohansinghpawar@users.noreply.github.com>
2022-08-23 18:40:08 +0530

    Create ReaderClass.java

---
+++ ReaderClass.java
@@ -0,0 +1,15 @@
+import java.time.*;
+public class GFG {
+	public static void main(String[] args)
+	{
+		LocalDate date = LocalDate.parse("2018-12-31");
+		System.out.println(date);
+		LocalDate returnvalue = date.plusMonths(9);
+		System.out.println(returnvalue);
+	}
+	Reader reader = new StringReader("mohan singh pawar");
+	for (int i = 0; i < 5; i++) {
+				ch = reader.read();
+				System.out.print((char)ch);
+			}
+}
