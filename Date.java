372822740a919be8c2165b319c31b7a9fb84dd6f
Mohansinghpawar <88230569+Mohansinghpawar@users.noreply.github.com>
2022-08-23 18:48:32 +0530

    Create Date.java

---
+++ Date.java
@@ -0,0 +1,11 @@
+import java.util.*;
+  
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Date date1 = new Date();
+        System.out.println("Current date is " + d1);
+        
+    }
+}
